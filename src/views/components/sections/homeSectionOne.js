24770c46c9849e7776567bb809338afbee28d380
Eduardo Andrade <eduardoandrade@me.com>
2021-07-15 22:56:45 -0300

    teste
    teste

---
+++ src/views/components/sections/homeSectionOne.js
@@ -0,0 +1,14 @@
+
+
+let HomeSectionOne = `
+<section id="section-one" class="section background--one">
+    <div>
+        <div class="container block-section--one">
+            <h1> Última sexta-feira de Novembro, na Galáxia em que você esta...</h1>
+            <h4> (clique no Baby Yoda ou use a Força)</h4>
+        </div>
+    </div>
+</section>
+`
+
+export default HomeSectionOne
---
+++ src/views/components/sections/homeSectionTwo.js
@@ -0,0 +1,28 @@
+import Logo from '../../../assets/images/logo/logo.png'
+import backgroundTwo from '../../../assets/images/background/bg2.png'
+
+let HomeSectionTwo = `
+<section id="section-two" class="section background--two" style="background-image: url(${backgroundTwo})">
+    <div>
+        <div class="container block-section--two">
+            <img src=${Logo} alt="Logo Star Wars" width="700px" height="auto">
+            <h2>A maior promoção do ano de produtos licenciados!</h2>
+            <br><br><br><br><br>
+            <h2>Para ofertas receber, cadastrar e-mail você precisa!</h2>
+            <form>
+            E-mail:<input type="email" name="email" id="email">
+            <button onclick="store()" type="button">Cadastrar e-mail</button>
+            <script  type="text/javascript">
+              function store(){
+                 var inputEmail= document.getElementById("email");
+                 localStorage.setItem("email", inputEmail.value);
+                }
+            </script>
+            
+            </form>
+        </div>
+    </div>
+</section>
+`
+
+export default HomeSectionTwo
