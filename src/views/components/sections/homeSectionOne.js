2c86af6dec93b9341aa1f5bacfe6d6f541c43f02
Eduardo Andrade <eduardoandrade@me.com>
2021-07-16 09:48:07 -0300

    Update homeSectionOne.js

--- src/views/components/sections/homeSectionOne.js
+++ src/views/components/sections/homeSectionOne.js
@@ -4,7 +4,7 @@ let HomeSectionOne = `
 <section id="section-one" class="section background--one">
     <div>
         <div class="container block-section--one">
-            <h1> Última sexta-feira de Novembro, na Galáxia em que você esta...</h1>
+            <h1> Última sexta-feira de Novembro, na Galáxia em que você está...</h1>
             <h4> (clique no Baby Yoda ou use a Força)</h4>
         </div>
     </div>
